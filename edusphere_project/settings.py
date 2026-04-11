defc5c2850cc2187bdb034c161738d074fe3e196
Niraj Upadhyay <nirajupadhyay221@gmail.com>
2026-04-11 19:26:32 +0530

    create admin

--- edusphere_project/settings.py
+++ edusphere_project/settings.py
@@ -162,3 +162,13 @@ JAZZMIN_UI_TWEAKS = {
         "success": "btn-success"
     }
 }
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="Admin@1234"
+    )
